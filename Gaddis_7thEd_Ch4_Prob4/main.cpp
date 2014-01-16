5ca6c5992d3977776c6230346c1b07c532abb696
davidBallantyne123@gmail.com <d1619123>
2014-01-16 11:52:54 -0800

    Signed-off-by: davidBallantyne123@gmail.com <d1619123>

---
+++ Gaddis_7thEd_Ch4_Prob4/main.cpp
@@ -0,0 +1,31 @@
+/*
+David Ballantyne
+01/16/14
+purpose:Gaddis_7thEd_Ch4_Prob4
+*/
+//Libraries
+#include <cstdlib>
+#include <iostream>
+#include <cmath>
+using namespace std;
+//Global Constants
+
+//Functioning Prototypes
+
+//Execution Begins Here
+int main(int argc, char *argv[]){
+    //Declare Variables
+    float l1, l2, w1,w2,a1,a2;
+    
+    //prompt user
+    cin>>l1,w1;
+    cout<<endl;
+    cin>>l2,w2;
+    //process
+    a1=l1*w1;
+    a2=l2*w2;
+    if (a1>a2 || a1==a2){
+    
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
